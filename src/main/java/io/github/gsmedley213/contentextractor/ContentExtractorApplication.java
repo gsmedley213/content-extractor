eca8fbea10e2e1e2db7e86ccad0fdc1df27ef3aa
gsmedley <gsmedley@gmail.com>
2025-05-23 09:20:49 -0400

    Spring Boot Initialzr code

---
+++ src/main/java/io/github/gsmedley213/contentextractor/ContentExtractorApplication.java
@@ -0,0 +1,13 @@
+package io.github.gsmedley213.contentextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gsmedley213/contentextractor/ContentExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gsmedley213.contentextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
